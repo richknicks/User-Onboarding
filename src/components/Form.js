d012c2fd6dfa3590bb88f681f8862e2757ad2975
richknicks <rlynch@gmail.com>
2020-01-31 02:58:14 -0500

    styled button

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,14 @@ import React, { useState, useEffect } from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
+import styled from "styled-components";
+
+const ButtonFace = styled.button`
+  margin: 20px auto;
+  width: 30%;
+  background-color: black;
+  color: white;
+`;
 
 const MyForm = ({ values, errors, touched, status }) => {
   const [person, setPerson] = useState([]);
@@ -10,62 +18,73 @@ const MyForm = ({ values, errors, touched, status }) => {
     status && setPerson(person => [...person, status]);
   }, [status]);
   return (
-    <div>
+    <div className="container">
       <Form>
         <label htmlFor="name">
-          Name
+          <span>Name: </span>
           <Field id="name" type="text" name="name" placeholder="name" />
-  {touched.name && errors.name &&(<p className="errors">{errors.name}</p>)}
+          {touched.name && errors.name && (
+            <p className="errors">{errors.name}</p>
+          )}
         </label>
         <label htmlFor="email">
-          Email
+          <span>Email: </span>
           <Field id="email" type="email" name="email" placeholder="email" />
-          {touched.email && errors.email &&(<p className="errors">{errors.email}</p>)}
+          {touched.email && errors.email && (
+            <p className="errors">{errors.email}</p>
+          )}
         </label>
         <label htmlFor="password">
-          Password
-          <Field id="password" type="password" name="password" placeholder="password" />
-          {touched.password && errors.password &&(<p className="errors">{errors.password}</p>)}
+          <span>Password: </span>
+          <Field
+            id="password"
+            type="password"
+            name="password"
+            placeholder="password"
+          />
+          {touched.password && errors.password && (
+            <p className="errors">{errors.password}</p>
+          )}
         </label>
         <label htmlFor="tos">
           Terms of Service
-          <Field type="checkbox" name="tos" checked={values.tos}/>
-          <span className="checkmark"/>
+          <Field type="checkbox" name="tos" checked={values.tos} />
+          <span className="checkmark" />
         </label>
-        <button type="submit">Submit</button>
+        <ButtonFace type="submit">Submit</ButtonFace>
       </Form>
+      
       {person.map(person => {
-          return(
-              <ul key={person.id}>
-                  <li>Name: {person.name}</li>
-                  <li>Email: {person.email}</li>
-                  <li>Password: {person.password}</li>
-                  <li>Terms of Service: {person.tos}</li>
-              </ul>
-          );
+        return (
+          <ul key={person.id}>
+            <li>Name: {person.name}</li>
+            <li>Email: {person.email}</li>
+            <li>Password: {person.password}</li>
+            <li>Terms of Service: {person.tos}</li>
+          </ul>
+        );
       })}
     </div>
   );
 };
 
 const FormikForm = withFormik({
-    mapPropsToValues(props){
+  mapPropsToValues(props) {
     return {
-        name: props.name || "",
-        email: props.email || "",
-        password: props.password || "",
-        tos: props.tos || false,
+      name: props.name || "",
+      email: props.email || "",
+      password: props.password || "",
+      tos: props.tos || false
     };
-},
-validationSchema: Yup.object().shape({
+  },
+  validationSchema: Yup.object().shape({
     name: Yup.string().required("*Required Field"),
     email: Yup.string().required("*Required Field"),
     password: Yup.string().required("*Required Field"),
     tos: Yup.boolean(true)
+  }),
 
-}),
-
-handleSubmit(values, { setStatus, resetForm }) {
+  handleSubmit(values, { setStatus, resetForm }) {
     console.log("submitting", values);
     axios
       .post("https://reqres.in/api/users/", values)
@@ -73,10 +92,9 @@ handleSubmit(values, { setStatus, resetForm }) {
         console.log("success", response);
         setStatus(response.data);
         resetForm();
-    })
-    .catch(err => console.log(err.response));
-}
+      })
+      .catch(err => console.log(err.response));
+  }
 })(MyForm);
-
 
 export default FormikForm;
